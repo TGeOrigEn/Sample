acd4fb4c5a71fcee982f8e78e8f3414742a7b5e0
Artem Tislo <tgeorigen@mail.ru>
2023-05-12 15:53:00 +0600

    Adding controller.

---
+++ Sample.WebAPI/Controllers/Controller.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Application.Entities;
+using Sample.Application.Requests.Queries;
+
+namespace Sample.WebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        protected IMediator Mediator => HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new Exception($"Service object of type {nameof(IMediator)} not existing.");
+
+        private readonly ILogger<Controller> _logger;
+
+        public Controller(ILogger<Controller> logger) => _logger = logger;
+
+        [HttpGet]
+        [Route("allData")]
+        public async Task<ActionResult<Log>> GetLog()
+        {
+            var query = new LogQuery();
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
