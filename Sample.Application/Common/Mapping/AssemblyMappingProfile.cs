dd1c745b20f203eda49a3a063d1054e5c68204c0
Artem Tislo <tgeorigen@mail.ru>
2023-05-12 13:38:30 +0600

    Adding class for automatic mapping profile assembly.

---
+++ Sample.Application/Common/Mapping/AssemblyMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Sample.Application.Common.Mapping
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingFromAssembly(assembly);
+
+        private void ApplyMappingFromAssembly(Assembly assembly) => assembly.GetExportedTypes()
+            .Where(IsAssignableFromMappingProfile).ToList()
+            .ForEach(InvokeMapping);
+
+        private static bool IsAssignableFromMappingProfile(Type type) =>
+            type.GetInterfaces().Any(i => i.IsAssignableFrom(typeof(IMappingProfile)));
+
+        private void InvokeMapping(Type type) =>
+            type.GetMethod("Mapping")?.Invoke(Activator.CreateInstance(type), new object[] { this });
+    }
+}
